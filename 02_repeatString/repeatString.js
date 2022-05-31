04c2e53efae8de8c208ceef69e953a0f997b79fb
Kedzie <kedziestark@gmail.com>
2022-05-31 14:40:04 -0400

    Create loop that passes first 4 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string, times) {
+    let repeatedString = "";
+      for (let i = 0; i < times; i++){
+          repeatedString += string;
+      }
+    return repeatedString
+  };
+  console.log(repeatString("hey",1));
 // Do not edit below this line
 module.exports = repeatString;
