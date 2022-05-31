23061f21ace8c90054b9ad8d9cf396420fc6f794
Kedzie <kedziestark@gmail.com>
2022-05-31 14:51:31 -0400

    Add error message for negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,26 @@
+// const repeatString = function(string, times) {
+//     let repeatedString = "";
+//     for (let i = 0; i < times; i++){
+//         repeatedString += string;
+//     }
+//     return repeatedString
+// };
+
+// console.log(repeatString("hey",1));
+
 const repeatString = function(string, times) {
-    let repeatedString = "";
-      for (let i = 0; i < times; i++){
-          repeatedString += string;
-      }
-    return repeatedString
-  };
-  console.log(repeatString("hey",1));
+    if (times < 0) {
+        return "ERROR";
+    } else {
+        let repeatedString = "";
+        for (let i = 0; i < times; i++){
+            repeatedString += string;
+        }
+        return repeatedString
+    }
+};
+
+console.log(repeatString("hey",-1));
+
 // Do not edit below this line
 module.exports = repeatString;
