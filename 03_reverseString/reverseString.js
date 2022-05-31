405d14efe1925afe9d0939f70a6736f6e4a8a924
Kedzie <kedziestark@gmail.com>
2022-05-31 15:25:37 -0400

    Create reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringLetters = string.split("");
+    stringLetters.reverse();
+    let reversedString = stringLetters.join("");
+    return reversedString;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
