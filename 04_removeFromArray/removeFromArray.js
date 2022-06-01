110273481432f7a5329b403032b0d61dc04c6dd8
Kedzie <kedziestark@gmail.com>
2022-06-01 13:09:31 -0400

    Create function that passes first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(input, toRemove) {
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] === toRemove) {
+        input.splice(i, 1);
+      }
+    }
+    return input;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
