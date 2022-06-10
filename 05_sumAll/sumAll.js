66b64dca88d3f15236e1ae126c8ab6c1fa79badb
Kedzie <kedziestark@gmail.com>
2022-06-10 16:59:32 -0400

    Create pseudocode

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
 
 };
+
+//Check parameter types
+    //if num1 or num 2 arent numbers return "ERROR"
+        //if (typeof num1 != "string" AND typeof num2 != "string" ){
+            //return "ERROR"
+        //}
+//check values
+    //if num1 or num2 < 0 return "ERROR"
+        //if num1 < 0 OR num2 < 0
+        //return "ERROR"
+//put paramaters in order- larger first
+    //if (num1 > num2) {
+        //let temp
+        //temp = num1
+        //num1 = num2
+        //num2 = temp
+    //}
+
+//once parameters are cleaned up and in order...
+//create variable to store sum
+    //let numSum = 0;
+//create array of range between num1 and num2
+//loop through resultant array and add items
+    //for const numItem of numRange {
+        //numSum = numSum + numItem
+    //}
+//return final sum
 
 // Do not edit below this line
 module.exports = sumAll;
