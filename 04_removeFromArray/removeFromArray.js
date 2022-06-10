5a75507ccae083f02cd739823401514a138ed4ef
Kedzie <kedziestark@gmail.com>
2022-06-10 15:47:41 -0400

    Add workaround for array mutating bug

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,15 @@
-const removeFromArray = function(input, toRemove) {
+const removeFromArray = function (input, ...toRemove) {
     for (let i = 0; i < input.length; i++) {
-      if (input[i] === toRemove) {
-        input.splice(i, 1);
-      }
+        for (let j = 0; j < toRemove.length; j++) {
+            if (input[i] === toRemove[j]) {
+                input.splice(i, 1);
+                i = -1; continue;
+            }
+        }
     }
     return input;
-  };
+};
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
