0e71ba4e23675d183e75287a62ce99daebcfdfda
Kedzie <kedziestark@gmail.com>
2022-06-13 13:32:10 -0400

    Create function that passes all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,33 +1,24 @@
-const sumAll = function(num1,num2) {
-
+const sumAll = function (num1, num2) {
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    } else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (num1 > num2) {
+        let temp;
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    let numRange = [];
+    for (i = num1; i <= num2; i++) {
+        numRange.push(i);
+    }
+    let numSum = 0;
+    for (const numItem of numRange) {
+        numSum = numSum + numItem;
+    }
+    return numSum;
 };
-
-//Check parameter types
-    //if num1 or num 2 arent numbers return "ERROR"
-        //if (typeof num1 != "string" AND typeof num2 != "string" ){
-            //return "ERROR"
-        //}
-//check values
-    //if num1 or num2 < 0 return "ERROR"
-        //if num1 < 0 OR num2 < 0
-        //return "ERROR"
-//put paramaters in order- larger first
-    //if (num1 > num2) {
-        //let temp
-        //temp = num1
-        //num1 = num2
-        //num2 = temp
-    //}
-
-//once parameters are cleaned up and in order...
-//create variable to store sum
-    //let numSum = 0;
-//create array of range between num1 and num2
-//loop through resultant array and add items
-    //for const numItem of numRange {
-        //numSum = numSum + numItem
-    //}
-//return final sum
 
 // Do not edit below this line
 module.exports = sumAll;
