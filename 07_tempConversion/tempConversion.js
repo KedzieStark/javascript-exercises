3cb7aa94c3d947181f05311446c8507398b4b62e
Kedzie <kedziestark@gmail.com>
2022-06-13 14:26:24 -0400

    Create conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp = (temp - 32) * .556;
+  temp = Math.round(temp * 10) / 10;
+  return temp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = temp * .556 +32;
+  temp = Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
