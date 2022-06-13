ab26cc698666b658254f80efd7384b125c390440
Kedzie <kedziestark@gmail.com>
2022-06-13 14:36:51 -0400

    Edit functions to pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,13 @@
 const ftoc = function(temp) {
-  temp = (temp - 32) * .556;
+  temp = (temp - 32) * 5/9;
   temp = Math.round(temp * 10) / 10;
   return temp;
 };
 
 const ctof = function(temp) {
-  temp = temp * .556 +32;
+  temp = temp * (9/5) + 32;
   temp = Math.round(temp * 10) / 10;
+  return temp;
 };
 
 // Do not edit below this line
