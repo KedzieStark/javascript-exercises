99da39296bd04f2620b91a4a1f09dbaee3c78c5e
Kedzie <kedziestark@gmail.com>
2022-07-07 17:17:38 -0400

    Create function that passes all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function(input) {
+    const alpha = Array.from("abcdefghijklmnopqrstuvwxyz");
+    input = input.toLowerCase();
+    let letterArray = Array.from(input).filter(letter => alpha.includes(letter));
+    let backwardsArray = letterArray.slice().reverse();
+    letterArray = letterArray.toString();
+    backwardsArray = backwardsArray.toString();
+    if (letterArray === backwardsArray) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
