18a7dfadf8f73e88f52b9295a0d20f090f7cd066
Kedzie <kedziestark@gmail.com>
2022-07-08 11:21:10 -0400

    Create function that passes test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(input) {
+    let result = input.map(book => book.title);
+    return result;
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
