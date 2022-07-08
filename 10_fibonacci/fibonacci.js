c8ba4c7de4014e121350863e8878fc63d75768c3
Kedzie <kedziestark@gmail.com>
2022-07-08 10:36:11 -0400

    Create function that passes all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (input) {
+    input = Number(input);
+    if (input == NaN || input < 0) {
+        return "OOPS";
+    } else {
+        const fibArray = [];
+        let a = 1, b = 0, temp;
+        for (let i = 0; i < input; i++) {
+            temp = a;
+            a = a + b;
+            b = temp;
+            fibArray[i] = temp;
+        }
+        return fibArray[input-1];
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
